83e482fedb68190efac72dbc724986b394ff7ad4
Vasili Gulevich <vasili.gulevich@xored.com>
2023-05-04 15:46:09 +0400

    Sort the output of why

--- why.py
+++ why.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+from functools import cache
 from feature import include_graph, find_node_by_id, print_sorted_with_meta
 from sys import argv
 from pprint import pprint
@@ -11,11 +12,38 @@ g = include_graph('.', include_dependencies=True)
 
 
 def content(source, target):
-	return set(ancestors(g, source)).intersection(descendants(g, target))
+	result = set(ancestors(g, source)).intersection(descendants(g, target))
+	result.add(source)
+	result.add(target)
+	return result
+
+
+def find_root(graph, node):
+	try:
+		return next(ancestors(graph, node))
+	except StopIteration:
+		return node
+
 
 if __name__ == '__main__':
 	result = set()
-	nodes = map(lambda id :  find_node_by_id(g, id), argv[1:])
-	for i, k in permutations(nodes, 2):
+	origin_nodes = map(lambda id:  find_node_by_id(g, id), argv[1:])
+	for i, k in permutations(origin_nodes, 2):
 		result.update(content(i, k))
-	print_sorted_with_meta(g, result)
+	#print(result)
+	subgraph = g.subgraph(result)
+#	for n, d in  subgraph.in_degree():
+#		print(n, d)
+	roots = set([n for n, d in subgraph.in_degree() if d == 0])
+	#print("Roots:", roots)
+	
+	@cache
+	def distance_from_root(node):
+		return len(list(ancestors(subgraph, node)))
+	
+	sorted = list(result)
+	sorted.sort(key=distance_from_root)
+	for node in sorted:
+		print (node, '\t', subgraph.nodes[node]['file'])
+	
+	#print_sorted_with_meta(g, result)
