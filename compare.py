7fc4b1ffe393932c8be865e9092aa5b88fb94c34
Vasili Gulevich <vasili.gulevich@xored.com>
2023-05-04 15:44:52 +0400

    Compare ancestors

--- compare.py
+++ compare.py
@@ -2,41 +2,48 @@
 from feature import include_graph, find_node_by_id, print_sorted
 from sys import argv
 from functools import cache
-from networkx import descendants
+from networkx import descendants, ancestors
 
-g = include_graph('.', include_dependencies = True)
+g = include_graph('.', include_dependencies=True)
+
+
+subjects = set([find_node_by_id(g, arg) for arg in argv[1:]])
+
+
+def compare_by(content, label):
+    print("Common", label + ":")
+    print_sorted(set.intersection(*[content(subject) for subject in subjects]))
+
+    for s in subjects:
+        print(label, "only from", s + ":")
+        included = set(content(s))
+        for another in subjects:
+            if another == s:
+                continue
+            included.difference_update(content(another))
+        print_sorted(included)
+
+    all_content = frozenset(
+        [c for subject in subjects for c in content(subject)])
+
+    @cache
+    def excluded_from(node):
+        return all_content.difference(content(node))
+
+    for s in subjects:
+        print(label, 'not in', s+':')
+        print_sorted(excluded_from(s).difference(
+            *[excluded_from(another) for another in subjects if another != s]))
+
 
 @cache
-def content(id):
-	return set(descendants(g, id))
+def dependencies(id):
+    return set(descendants(g, id))
 
-
-subjects = set([ find_node_by_id(g, arg) for arg in argv[1:] ])
-
-
-print("Shared dependencies:")
-print_sorted(set.intersection(*[content(subject) for subject in subjects])) 
-
-for s in subjects:
-	print('Included only in', s, ':')
-	included = set(content(s))
-	for another in subjects:
-		if another == s:
-			continue
-		included.difference_update(content(another))
-	print_sorted(included)
-	
-all_content = frozenset([c for subject in subjects for c in content(subject) ] )
 
 @cache
-def excluded_from(node):
-	return all_content.difference(content(node))
-	
-for s in subjects:
-	print('Excluded only from', s, ':')
-	print_sorted(excluded_from(s).difference(*[ excluded_from(another) for another in subjects if another != s ]))
-	
+def parents(id):
+    return set(ancestors(g, id))
 
-
-#print('Shared:')
-#print_sorted(content(next(iter(subjects))).intersection(*[content(i) for i in subjects]))
+compare_by(parents, 'anscestors')
+compare_by(dependencies, 'dependencies')
